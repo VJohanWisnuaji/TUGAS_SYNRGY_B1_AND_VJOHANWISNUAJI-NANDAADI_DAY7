0dffd64d7193c75adf818f2971b6438142135db3
VJohanWisnuaji <wisnuaji.johan@gmail.com>
2020-07-01 14:11:56 +0700

    Tambahan menu atm

--- src/Main.kt
+++ src/Main.kt
@@ -11,29 +11,24 @@ fun main() {
         } else {
             if (inputPin.length != 6) {
                 println("Panjang PIN salah")
-                print("Please input password: ")
-                var inputPin: String = readLine()!!
                 return main()
             } else {
                 if (inputPin != "123456") {
                     println("PIN salah")
-                    print("Please input password: ")
-                    var inputPin: String = readLine()!!
                     return main()
                 } else {
-                    println("cek")
+                    atm()
                 }
             }
         }
     }
 
     checkPin(inputPin)
-    atm()
+
 }
 
-fun atm(){
+fun atm() {
     val atm = ATM()
-
     println(" ")
     println("------------------------------")
     println("== LIST MENU ATM  ==")
@@ -41,4 +36,99 @@ fun atm(){
     println("A. Cek Saldo")
     println("B. Tarik Tunai")
     println("C. Setor Tunai")
+    println("D. Selesai")
+    println("Note: tulis A,B,C untuk memilih menu")
+    print("Masukkan pilihan menu: ")
+    var inputMenu: String = readLine()!!
+
+
+    when (inputMenu) {
+        "A" -> {
+            println("-------------------------===")
+            println("-----===CEK SALDO-----===")
+            println("-------------------------===")
+
+            var saldo = ATM().saldo
+            var separator = saldo/1000
+            println("Saldo pada ATM anda adalah Rp. ${saldo}")
+
+
+            println("Kembali ke menu?? (Yes/No) ")
+            val checkNextStudy: String = readLine()!!
+            if (checkNextStudy == "Yes") {
+                return atm()
+            } else {
+                println("-----  ---------------------------------  -----")
+                println("-----  Terimakasih sudah menggunakan ATM  -----")
+                println("-----  ---------------------------------  -----")
+            }
+        }
+        "B" -> {
+//                println("-------------------------===")
+//                menuCallculator.casePengurangan()
+//                println("-------------------------===")
+//
+//                val calc = JenisPerhitungan()
+//                val pengurangan = Pengurangan(userInputX = 0.0, userInputY= 0.0)
+//
+//                print(" userInput X: ")
+//                pengurangan.userInputX = Scanner(System.`in`).nextDouble()
+//
+//                print(" userInput y: ")
+//                pengurangan.userInputY = Scanner(System.`in`).nextDouble()
+//
+//
+//                calc.penguranganFunction()
+//                pengurangan.penguranganFunction()
+
+            println("Kembali ke menu?? (Yes/No) ")
+            val checkNextStudy: String = readLine()!!
+            if (checkNextStudy == "Yes") {
+                return atm()
+            } else {
+                println("-----  ---------------------------------  -----")
+                println("-----  Terimakasih sudah menggunakan ATM  -----")
+                println("-----  ---------------------------------  -----")
+            }
+        }
+        "C" -> {
+//                println("-------------------------===")
+//                menuCallculator.casePerkalian()
+//                println("-------------------------===")
+//
+//                val calc = JenisPerhitungan()
+//                val perkalian = Perkalian(userInputX = 0.0, userInputY= 0.0)
+//
+//                print(" userInput X: ")
+//                perkalian.userInputX = Scanner(System.`in`).nextDouble()
+//
+//                print(" userInput y: ")
+//                perkalian.userInputY = Scanner(System.`in`).nextDouble()
+//
+//
+//                calc.perkalianFunction()
+//                perkalian.perkalianFunction()
+
+            println("Kembali ke menu?? (Yes/No) ")
+            val checkNextStudy: String = readLine()!!
+            if (checkNextStudy == "Yes") {
+                return atm()
+            } else {
+                println("-----  ---------------------------------  -----")
+                println("-----  Terimakasih sudah menggunakan ATM  -----")
+                println("-----  ---------------------------------  -----")
+            }
+        }
+        "D" -> {
+            println("-----  ---------------------------------  -----")
+            println("-----  Terimakasih sudah menggunakan ATM  -----")
+            println("-----  ---------------------------------  -----")
+        }
+        else -> {
+            println(" Check note untuk cara memilih menu!")
+            return atm()
+        }
+    }
+
+
 }
